a6b9ea61acf9ffc6e3bf1df9330c97601b78b74b
Skinz3 <oliviamarius35@gmail.com>
2021-08-18 04:42:29 +0200

    Remove build & batch

--- StartGame.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\Skinz\Desktop\Emulateur LoL\League of Legends 4.20\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.68\deploy\"
-
-@start "" "League of Legends.exe" "8394" "LoLLauncher.exe" "" "130.211.97.242 5119 17BLOhi6KZsTtldTsizvHg== 1"
